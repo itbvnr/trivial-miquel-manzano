7d32a958552d0f0a6fa9fc1255bc44449ea60335
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-02-20 10:45:36 +0100

    Modifiers

--- composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/ScoreApp.kt
+++ composeApp/src/commonMain/kotlin/cat/itb/m78/exercices/ScoreApp.kt
@@ -1,8 +1,10 @@
 // Essential Imports
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 // ViewModel Imports
 import androidx.lifecycle.ViewModel
@@ -27,20 +29,28 @@ class MyViewModel : ViewModel() {
 // fun MenuScreen(addCount: () -> Unit) Asi se pasa una funcion, y si la asignamos al OnClick la ejecutara
 fun MenuScreen() {
     val myViewModel = viewModel { MyViewModel() }
-    Row(
+    Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxSize()
+            .background(Color.LightGray),
+        verticalArrangement = Arrangement.Center
     ) {
-        Column {
-            Text(myViewModel.local.value.toString())
-            Button(onClick = myViewModel::AddScoreLocal){
-                Text("Score")
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Column {
+                Text(myViewModel.local.value.toString())
+                Button(onClick = myViewModel::AddScoreLocal){
+                    Text("Score")
+                }
             }
-        }
-        Column {
-            Text(myViewModel.visit.value.toString())
-            Button(onClick = myViewModel::AddScoreVisit ){
-                Text("Score")
+            Column {
+                Text(myViewModel.visit.value.toString())
+                Button(onClick = myViewModel::AddScoreVisit ){
+                    Text("Score")
+                }
             }
         }
     }
